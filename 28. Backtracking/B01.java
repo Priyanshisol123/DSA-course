ffa7525985d556093433c5040774de07affe33bf
Priyanshi Solanki <priyanshi082005@gmail.com>
2026-01-22 20:13:05 +0530

    backtracking permutation in string, string subset and decreasing element in array

---
+++ 28. Backtracking/B01.java
@@ -0,0 +1,25 @@
+//In an Array Decrease every element value by 2
+public class B01{
+    public static void changeArr(int arr[],int i,int val){
+        //base case
+        if(i==arr.length){
+            printArr(arr);
+            return;
+        }
+        //recursion
+        arr[i]=val;
+        changeArr(arr,i+1,val+1);//function call
+        arr[i]=arr[i]-2;//Backtracking
+    }
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        } 
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[]=new int[5];
+        changeArr(arr,0,1);
+        printArr(arr);
+    }
+}
