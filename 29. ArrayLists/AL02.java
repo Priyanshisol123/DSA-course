af7b0ee01caef31bc31c76ba80521671702ff7a1
Priyanshi Solanki <priyanshi082005@gmail.com>
2026-02-02 20:00:32 +0530

    Arraylist reverse

---
+++ 29. ArrayLists/AL02.java
@@ -0,0 +1,19 @@
+//Print array list in reverse
+import java.util.ArrayList;
+public class AL02{
+    public static void main (String args[]){
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        //reverse print arrayList
+        for(int i=list.size()-1;i>=0;i--){
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+    }
+}
