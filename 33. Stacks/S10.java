084f13a32c96482697ac5e83c6fcad0aa84fc7c0
Priyanshi Solanki <priyanshi082005@gmail.com>
2026-03-11 01:45:39 +0530

    Stack maxArea

---
+++ 33. Stacks/S10.java
@@ -0,0 +1,58 @@
+//Max Area in Histogram
+//Given an array of integers heights representing the 
+//histogram's bar height where the width of each bar 
+//is 1, return the area of the largest rectangle in 
+//the histogram.       heights[2,1,5,6,2,3]     ans=10
+import java.util.*;
+public class S10{
+    public static void maxArea(int arr[]){
+        int maxArea=0;
+        int nsr[]=new int[arr.length];
+        int nsl[]=new int[arr.length];
+
+        //Next Smaller Right=O(n)
+        Stack<Integer> s=new Stack<>();
+
+        for(int i=arr.length-1;i>=0;i--){
+            while(!s.isEmpty() && arr[s.peek()]>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsr[i]=arr.length;
+            }else{
+                nsr[i]=s.peek();
+            }
+            s.push(i);
+        }
+
+        //Next Smaller Left=O(n)
+        s=new Stack<>();//khali kiya stack
+
+        for(int i=0;i<arr.length;i++){
+            while(!s.isEmpty() && arr[s.peek()]>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsl[i]=-1;
+            }else{
+                nsl[i]=s.peek();
+            }
+            s.push(i);
+        }
+
+        //Current Area: width=j-i-1 = nsr[i] - nsl[i] - 1
+        for(int i=0;i<arr.length;i++){//O(n)
+            int height=arr[i];
+            int width=nsr[i]-nsl[i]-1;
+            int currArea=height*width;
+            maxArea=Math.max(currArea,maxArea);
+        }
+        System.out.println("max area in histogram = "+maxArea);
+    }
+    public static void main (String args[]){
+        int arr[]={2, 1, 5, 6, 2, 3};//heights in histogram
+        maxArea(arr);
+        int arr2[]={2, 4};
+        maxArea(arr2);
+    }
+}
