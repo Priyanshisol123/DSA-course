2f882a88ba67b02416bd2f1e7803fb342048944b
Priyanshi Solanki <priyanshi082005@gmail.com>
2026-03-18 22:01:31 +0530

    greedy q4

---
+++ 35. Greedy Algorithms/G04.java
@@ -0,0 +1,26 @@
+//Max Length Chain of Pairs
+//You are given n pairs of numbers. In every pair,the 
+//first number is always smaller than the second number.
+//A pair (c,d) can come after pair (a,b) if b<c. 
+//          Find the longest chain which can be formed 
+//from a given set of pairs.                 ans = 3
+//pairs=(5,24) (39,60) (5,28) (27,40) (50,90)
+import java.util.*;
+public class G04{
+    public static void main(String args[]){//O(nlogn)
+        int pairs[][]={{5,24},{39,60},{5,28},{27,40},{50,90}};
+
+        Arrays.sort(pairs,Comparator.comparingDouble(o->o[1]));
+
+        int chainLen=1;
+        int chainEnd=pairs[0][1];//Last selected pair end //chain end
+
+        for(int i=1;i<pairs.length;i++){
+            if(pairs[i][0]>chainEnd){
+                chainLen++;
+                chainEnd=pairs[i][1];
+            }
+        }
+        System.out.println("max length of chain = "+chainLen);
+    }
+}
