d82b3b81477cd60db6d5e208949afe5846400a24
Priyanshi Solanki <priyanshi082005@gmail.com>
2026-03-21 01:00:05 +0530

    tries complete

---
+++ 43. Tries/T01.java
@@ -0,0 +1,74 @@
+//Creating Tries,insert,search,Word Break Problem
+public class T01{
+    static class Node{
+        Node children[]=new Node[26];
+        boolean eow=false;
+
+        Node(){
+            for(int i=0;i<26;i++){
+                children[i]=null;
+            }
+        }
+    }
+    public static Node root=new Node();
+
+    //insert
+    public static void insert(String word){//O(L)
+        Node curr=root;
+        for(int level =0;level<word.length();level++){
+            int idx =word.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                curr.children[idx]=new Node();
+            }
+            curr=curr.children[idx];
+        }
+        curr.eow=true;
+    }
+
+    //search
+    public static boolean search(String key){
+        Node curr=root;
+        for(int level=0;level<key.length();level++){
+            int idx=key.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                return false;
+            }
+            curr=curr.children[idx];
+        }
+        return curr.eow==true;
+    }
+
+    //Word Break Problem
+    public static boolean wordBreak(String key){//O(n)
+        if(key.length()==0){
+            return true;
+        }
+        for(int i=1;i<=key.length();i++){
+            if(search(key.substring(0,i)) &&
+                wordBreak(key.substring(i))){//(beginning idx,end idx)
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    public static void main(String args[]){
+        //Words break problem
+        String words[]={"i","like","sam","samsung","mobile","ice"};
+        for(int i=0;i<words.length;i++){
+            insert(words[i]);
+        }
+        String key="ilikesamsung";
+        System.out.println(wordBreak(key));
+
+        //These are from insert search operation and creation basics (below)
+
+        // String words[]={"the","a","there","their","any","thee"};
+        // for(int i=0;i<words.length;i++){
+        //     insert(words[i]);
+        // }
+
+        // System.out.println(search("thee"));
+        // System.out.println(search("thor"));
+    }
+}
